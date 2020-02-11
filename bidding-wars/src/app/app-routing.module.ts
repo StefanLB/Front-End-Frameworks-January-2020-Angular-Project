ebba2997ee5a4b052831d149a4564074ca5a248d
StefanLB <s_baliev@yahoo.com>
2020-02-12 00:01:33 +0200

    Small redirect tweaks - redirect to not found

--- bidding-wars/src/app/app-routing.module.ts
+++ bidding-wars/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const routes: Routes = [
-  { path: 'app-login', component: LoginComponent, /*canActivate: */},
+  { path: 'app-login', component: LoginComponent /*canActivate: */},
   { path: 'app-register', component: RegisterComponent, /*canActivate: [SecureInnerPagesGuard]*/ },
-  { path: '', redirectTo: '/app-login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/not-found'}
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
