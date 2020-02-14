14103614bcf1ec4caec32fbae658b414b56bb52f
Stefan Baliev <stefan.baliev@paysafe.com>
2020-02-14 12:16:57 +0200

    Fixed Login Page, w/o model.

--- bidding-wars/src/app/auth/login/login.component.ts
+++ bidding-wars/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,29 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  hide = true;
 
-  constructor(public authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+    ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      'email': [ '', [
+        Validators.required,
+        Validators.email
+      ]],
+      'password': ['', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(30)
+      ]]
+    });
   }
 
+  loginUser(){
+    this.authService.login(this.loginForm.value.email, this.loginForm.value.password);
+  }
 }
