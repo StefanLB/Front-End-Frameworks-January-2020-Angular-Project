d258a5d3162d37c2750f2922b4de8d74dfc763b9
StefanLB <s_baliev@yahoo.com>
2020-02-16 15:25:35 +0200

    All bids view now displays test data.

--- bidding-wars/src/app/bids/all-bids/all-bids-datasource.ts
+++ bidding-wars/src/app/bids/all-bids/all-bids-datasource.ts
@@ -3,35 +3,37 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { map } from 'rxjs/operators';
 import { Observable, of as observableOf, merge } from 'rxjs';
-
-// TODO: Replace this with your own data model type
-export interface AllBidsItem {
-  name: string;
-  id: number;
-}
+import { Bid } from '../bid';
 
 // TODO: replace this with real data from your application
-const EXAMPLE_DATA: AllBidsItem[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
+const EXAMPLE_DATA: Bid[] = [
+  {id: '1', name: 'Hydrogen'  , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 150},
+  {id: '2', name: 'Helium'    , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 150},
+  {id: '3', name: 'Lithium'   , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 150},
+  {id: '4', name: 'Beryllium' , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 150},
+  {id: '5', name: 'Boron'     , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 150},
+  {id: '6', name: 'Carbon'    , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 150},
+  {id: '7', name: 'Nitrogen'  , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 150},
+  {id: '8', name: 'Oxygen'    , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 150},
+  {id: '9', name: 'Fluorine'  , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 150},
+  {id: '10', name: 'Hydrogen'  , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 160},
+  {id: '20', name: 'Helium'    , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 160},
+  {id: '30', name: 'Lithium'   , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 160},
+  {id: '40', name: 'Beryllium' , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 160},
+  {id: '50', name: 'Boron'     , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 160},
+  {id: '60', name: 'Carbon'    , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 160},
+  {id: '70', name: 'Nitrogen'  , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 160},
+  {id: '80', name: 'Oxygen'    , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 160},
+  {id: '90', name: 'Fluorine'  , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 160},
+  {id: '100', name: 'Hydrogen'  , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 170},
+  {id: '200', name: 'Helium'    , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 170},
+  {id: '300', name: 'Lithium'   , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 170},
+  {id: '400', name: 'Beryllium' , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 170},
+  {id: '500', name: 'Boron'     , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 170},
+  {id: '600', name: 'Carbon'    , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 170},
+  {id: '700', name: 'Nitrogen'  , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 170},
+  {id: '800', name: 'Oxygen'    , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 170},
+  {id: '900', name: 'Fluorine'  , createdOn: new Date(), endsOn: new Date(), imageUrl: 'https://test.com', seller: 'testSeller', highestBid: 170}
 ];
 
 /**
@@ -39,8 +41,8 @@ const EXAMPLE_DATA: AllBidsItem[] = [
  * encapsulate all logic for fetching and manipulating the displayed data
  * (including sorting, pagination, and filtering).
  */
-export class AllBidsDataSource extends DataSource<AllBidsItem> {
-  data: AllBidsItem[] = EXAMPLE_DATA;
+export class AllBidsDataSource extends DataSource<Bid> {
+  data: Bid[] = EXAMPLE_DATA;
   paginator: MatPaginator;
   sort: MatSort;
 
@@ -53,7 +55,7 @@ export class AllBidsDataSource extends DataSource<AllBidsItem> {
    * the returned stream emits new items.
    * @returns A stream of the items to be rendered.
    */
-  connect(): Observable<AllBidsItem[]> {
+  connect(): Observable<Bid[]> {
     // Combine everything that affects the rendered data into one update
     // stream for the data-table to consume.
     const dataMutations = [
@@ -77,7 +79,7 @@ export class AllBidsDataSource extends DataSource<AllBidsItem> {
    * Paginate the data (client-side). If you're using server-side pagination,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getPagedData(data: AllBidsItem[]) {
+  private getPagedData(data: Bid[]) {
     const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
     return data.splice(startIndex, this.paginator.pageSize);
   }
@@ -86,7 +88,7 @@ export class AllBidsDataSource extends DataSource<AllBidsItem> {
    * Sort the data (client-side). If you're using server-side sorting,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getSortedData(data: AllBidsItem[]) {
+  private getSortedData(data: Bid[]) {
     if (!this.sort.active || this.sort.direction === '') {
       return data;
     }
@@ -96,6 +98,11 @@ export class AllBidsDataSource extends DataSource<AllBidsItem> {
       switch (this.sort.active) {
         case 'name': return compare(a.name, b.name, isAsc);
         case 'id': return compare(+a.id, +b.id, isAsc);
+        case 'createdOn': return compare(a.createdOn, b.createdOn, isAsc);
+        case 'endsOn': return compare(a.endsOn, b.endsOn, isAsc);
+        case 'imageUrl': return compare(a.imageUrl, b.imageUrl, isAsc);
+        case 'seller': return compare(a.seller, b.seller, isAsc);
+        case 'highestBid': return compare(+a.highestBid, +b.highestBid, isAsc);
         default: return 0;
       }
     });
--- bidding-wars/src/app/bids/all-bids/all-bids.component.ts
+++ bidding-wars/src/app/bids/all-bids/all-bids.component.ts
@@ -2,7 +2,9 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
-import { AllBidsDataSource, AllBidsItem } from './all-bids-datasource';
+import { AllBidsDataSource } from './all-bids-datasource';
+import { Bid } from '../bid';
+import { BidsService } from '../bids.service';
 
 @Component({
   selector: 'app-all-bids',
@@ -12,11 +14,18 @@ import { AllBidsDataSource, AllBidsItem } from './all-bids-datasource';
 export class AllBidsComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: false}) sort: MatSort;
-  @ViewChild(MatTable, {static: false}) table: MatTable<AllBidsItem>;
+  @ViewChild(MatTable, {static: false}) table: MatTable<Bid>;
   dataSource: AllBidsDataSource;
 
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = [
+  'id',
+  'name',
+  'createdOn',
+  'endsOn',
+  'imageUrl',
+  'seller',
+  'highestBid'
+  ];
 
   ngOnInit() {
     this.dataSource = new AllBidsDataSource();
