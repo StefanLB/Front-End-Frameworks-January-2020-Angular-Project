7606f40c46e50955276ffae39ca2d1d8957bd9d7
StefanLB <s_baliev@yahoo.com>
2020-02-18 23:56:15 +0200

    Sample bids are displayed for anonymous users, styling is presentable.

--- bidding-wars/src/app/home/home.component.ts
+++ bidding-wars/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BidsService } from '../bids/bids.service';
+import { Bid } from '../bids/bid';
+
 
 @Component({
   selector: 'app-home',
@@ -7,10 +10,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  sampleBids: Bid[] = [];
 
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private bidApi: BidsService
+  ) { 
+    this.bidApi.getBidsList()
+    .snapshotChanges().subscribe(bids => {
+      bids.forEach(item => {
+          let a = item.payload.toJSON();
+          a['$key'] = item.key;
+          this.sampleBids.push(a as Bid)
+        })
+      });
+  }
 
   ngOnInit() {
   }
